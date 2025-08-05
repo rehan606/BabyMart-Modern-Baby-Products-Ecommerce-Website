db5e25413759899e8dcdc914a46f532cc4494624
Rehan <rehanlemu@gmail.com>
2025-08-05 14:54:11 +0600

    Added register a new user method in authController.js file

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,12 +1,37 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 const loginUser = (req, res) => {
     res.send("Login Controller is working");
 }; 
 
+
+// Register a new user
+// POST /api/auth/register
 const registerUser = asyncHandler(async(req, res) => {
     const {name, email, password, role} = req.body;
-    console.log(name, email, password, role);
+    
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role,
+        addresses: [],
+    });
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            role: user.role,
+            addresses: user.addresses,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
 
     res.send("Register Controller is working")
 })
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -17,7 +17,7 @@ const userSchema = mongoose.Schema({
     role: {
         type: String,
         enum: ["admin", "user", "deliveryman"],
-        default: user,
+        default: "user",
     },
     addresses: [
         {
