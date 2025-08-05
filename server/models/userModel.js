c98481ef0b637730d05a73f1b4485b450548a7ab
Rehan <rehanlemu@gmail.com>
2025-08-05 14:13:30 +0600

    Create userSchema in userModel.js file

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -13,7 +13,44 @@ const userSchema = mongoose.Schema({
     avatar: {
         type: String,
         default: "https://www.gravatar.com/avatar/0000000000000000000000000000000?d=mp&f=y",
-    }
-    
-})
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user", "deliveryman"],
+        default: user,
+    },
+    addresses: [
+        {
+            street: {
+                type: String,
+                required: true,
+            },
+            city: {
+                type: String,
+                required: true,
+            },
+            country: {
+                type: String,
+                required: true,
+            },
+            postalCode: {
+                type: String,
+                required: true,
+            },
+            isDefault: {
+                type: Boolean,
+                default: false,
+            }
+        }
+    ],
+    wishlist: [],
+    cart: [],
 
+
+    
+},{
+    timestamps: true,
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
