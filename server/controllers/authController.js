99992236eac75ea3ed380f7f0e90a27578f67c7f
Rehan <rehanlemu@gmail.com>
2025-08-08 21:59:34 +0600

    Login user functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,26 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 
-const loginUser = (req, res) => {
-    res.send("Login Controller is working");
-}; 
+const loginUser = asyncHandler (async(req, res) => {
+    const {email, password} = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+
+    if(user && user.matchPassword(password)) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            role: user.role,
+            addresses: user.addresses || [],
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+})
 
 
 // Register a new user
