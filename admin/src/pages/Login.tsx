aa911b882ceb3fc3bb582189a6c579e399c59c0f
Rehan <rehanlemu@gmail.com>
2025-08-10 02:30:14 +0600

    Added motion effect in login component element

--- admin/src/pages/Login.tsx
+++ admin/src/pages/Login.tsx
@@ -1,22 +1,40 @@
-import { Card, CardHeader, CardTitle , CardDescription} from "@/components/ui/card";
-
+import { Card, CardHeader, CardTitle , CardDescription,  } from "@/components/ui/card";
+import { motion } from "motion/react"
 
 const Login = () => {
   return (
     <div className="min-h-screen w-full bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center">
-      <div className="w-full max-w-md px-4">
+      <motion.div 
+        initial={{opacity:0, y:200}} 
+        animate={{opacity: 1, y:0}}
+        transition={{ duration: 0.5, ease: "easeIn"}}
+        className="w-full max-w-md px-4">
         <Card className="w-full  bg-white/95 backdrop-blur-sm shadow-xl border border-gray-200"> 
           <CardHeader className="text-center space-y-2">
-            <div>
-              <CardTitle className="text-3xl font-bold text-gray-800"> Admin Dashboard</CardTitle>
-             </div>
+            <motion.div 
+              initial={{ scale: 0.5}}
+              animate={{ scale: 1}}
+              transition={{ duration: 0.3 }}
+              >
+              <CardTitle className="text-3xl font-bold text-gray-800 "> Admin Dashboard</CardTitle>
+              <CardDescription className="text-gray-500 text-center ">
+                Enter your credentials to sign in
+              </CardDescription>
+            </motion.div>
           </CardHeader>
 
-          <CardDescription className="text-gray-500 ">
-            Enter your credentials to sign in
-          </CardDescription>
+          {/* <CardContent>
+            <Form>
+              <form>
+                <FromField>
+
+                <FromField>
+              </form>
+            </Form>
+          </CardContent> */}
+          
         </Card>
-      </div>
+      </motion.div>
     </div>
   )
 }
