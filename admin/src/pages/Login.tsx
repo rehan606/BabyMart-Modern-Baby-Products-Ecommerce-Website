d0501e375e965aabc075b46df658ae2e7b7e1f8a
Rehan <rehanlemu@gmail.com>
2025-08-10 02:09:05 +0600

    install motion package for animation

--- admin/src/pages/Login.tsx
+++ admin/src/pages/Login.tsx
@@ -1,13 +1,25 @@
-import CommonLayout from "@/components/common/CommonLayout"
-import { Link } from "react-router"
+import { Card, CardHeader, CardTitle , CardDescription} from "@/components/ui/card";
 
 
 const Login = () => {
   return (
     <div className="min-h-screen w-full bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center">
-      <div>
-        
+      <div className="w-full max-w-md px-4">
+        <Card className="w-full  bg-white/95 backdrop-blur-sm shadow-xl border border-gray-200"> 
+          <CardHeader className="text-center space-y-2">
+            <div>
+              <CardTitle className="text-3xl font-bold text-gray-800"> Admin Dashboard</CardTitle>
+             </div>
+          </CardHeader>
+
+          <CardDescription className="text-gray-500 ">
+            Enter your credentials to sign in
+          </CardDescription>
+        </Card>
       </div>
     </div>
   )
 }
+
+export default Login
+
