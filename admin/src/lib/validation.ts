abad701b4af34c8215f1bbfd8b6ff26d538a590a
Rehan <rehanlemu@gmail.com>
2025-08-14 11:35:35 +0600

    Create registeSchema in admin side validation.ts file

--- admin/src/lib/validation.ts
+++ admin/src/lib/validation.ts
@@ -1,6 +1,19 @@
 import {z} from "zod"
 
-export const loginSchema=z.object({
+// Login Schema
+export const loginSchema = z.object({
     email:z.string().email({message:"Please enter a valid email address"}),
     password:z.string().min(6,{message:"Password must be at least 6 characters"})
-})
+});
+
+
+// Register Schema
+
+export const registerSchema = z.object({
+    name: z.string().min(2, {message: "Name must be at least 2 characters"}),
+    email: z.string().email({message:"Please enter a valid email address"}),
+    password: z.string().min(6,{message:"Password must be at least 6 characters"}),
+    role: z.enum(["admin", "user", "deliveryman"],{
+        message: "Please select a valid role",
+    })
+});
