642f3e63ceb7685fd0132be59ef60fcf0f724a8c
Rehan <rehanlemu@gmail.com>
2025-08-14 20:27:09 +0600

    create useAuthStore.ts file and add AuthState

---
+++ admin/src/store/useAuthStore.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand';
+import { persist } from "zustand/middleware"; // persist for local storage
+
+type User = {
+    _id:string;
+    name:string;
+    email:string;
+    avatar:string;
+    role:"admin" | "user" | "deliveryman"
+}
+
+type AuthState = {
+    user: User | null;
+    token:string | null;
+    isAuthenticated:boolean;
+    login:(Credentials:{email:string; password:string})=> Promise<void>;
+    register: (userData: {
+        name: string;
+        email: string;
+        password: string;
+        role: string;
+    }) => Promise<void>;
+
+    logout: () => void;
+    checkIsAdmin:()=>boolean
+};
+
+const useAuthStore = create<AuthState>() (persist((set, get)=>({
+    user: null
+}), {
+    name: "auth-storage"
+}
+))
