41f8abaadb467ddc3dba658df2c737220ca5b865
Rehan <rehanlemu@gmail.com>
2025-10-22 23:16:41 +0600

    Create orderModel.js file and create orderSchema

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -24,5 +24,59 @@ const orderItemSchema = new mongoose.Schema(
             type: String,
         },
         
-    }
+    },
 );
+
+const orderSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        items: [orderItemSchema],
+        total: {
+            type: Number,
+            default: 0,
+        },
+        status: {
+            type: String,
+            enum: ["Pending", "Processing", "Shipped", "Delivered", "Cancelled"],
+            default: "Pending"
+        },
+        shippingAddress: {
+            street: {
+                type: String,
+                required: true
+            },
+            city: {
+                type: String,
+                required: true
+            },
+            country: {
+                type: String,
+                required: true
+            },
+            postalCode: {
+                type: String,
+                required: true
+            },
+        },
+        paymentIntentId: {
+            type: String,
+        },
+        stripeSessionId: {
+            type: String,
+        },
+        paidAt: {
+            type: Date,
+        },
+    },
+    {
+        timestamps: true
+    },
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
