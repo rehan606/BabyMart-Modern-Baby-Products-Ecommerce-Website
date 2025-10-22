83835ea8d3e1d0f594146cc54cf8cb8f7815b882
Rehan <rehanlemu@gmail.com>
2025-10-23 01:18:02 +0600

    Create brands in server

---
+++ server/controllers/brandController.js
@@ -0,0 +1,37 @@
+import asyncHandler from "express-async-handler";
+import Brand from "../models/brandModel.js";
+import cloudinary from "../config/cloudinary.js";
+
+
+// Create Brand
+const createBrand = asyncHandler(async(req,res)=> {
+    const {name, image} = req.body
+
+    const brandExists = await Brand.findOne({ name });
+
+    if(brandExists){
+        res.status(400);
+        throw new Error ("Brand already exists , try different Brand name")
+    }
+
+    let imageUrl = ""
+
+    if(image){
+        const result = await cloudinary.upload(image,{
+            folder: "babymart/brands",
+        });
+        imageUrl = result.secure_url;
+    }
+    const brand = await Brand.create({
+        name,
+        image: imageUrl || "",
+    });
+    if (brand) {
+        res.status(201).json(brand);
+    } else {
+        res.status(400);
+        throw new Error("Invalid brand data");
+    }
+});
+
+export { createBrand };
--- server/routes/brandRoutes.js
+++ server/routes/brandRoutes.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { admin, protect } from "../middleware/authMiddleware.js";
+import { createBrand } from "../controllers/brandController.js";
 
 const router = express.Router();
 
+// Home Route
+router.route("/").get(getBrands).post(protect,admin,createBrand, )
 
+// :id
 
 export default router;
