51704b12cb641ba7deeecff68c94843563ecb7bb
Rehan <rehanlemu@gmail.com>
2025-10-22 23:05:55 +0600

    Create cartModel.js file and create cartrSchema

---
+++ server/models/orderModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema(
+    {
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product", 
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1
+        },
+        image: {
+            type: String,
+        },
+        
+    }
+);
