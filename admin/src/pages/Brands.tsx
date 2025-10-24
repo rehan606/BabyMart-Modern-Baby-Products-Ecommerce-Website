37a3bc1aa9ca1966e1abebff7bf3fe22f2253b24
Rehan <rehanlemu@gmail.com>
2025-10-24 21:16:36 +0600

    added state in admin Brands page

--- admin/src/pages/Brands.tsx
+++ admin/src/pages/Brands.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 
 
 const Brands = () => {
+
+  const [brands, setBrands] = useState<Brand[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false); 
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [selectedBrand, setSelectedBrand] = useState<Brand | null>(null);
+  const [formLoading, setFormLoading] = useState(false);
+
+
   return (
     <div>
       Brands
--- server/routes/brandRoutes.js
+++ server/routes/brandRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { admin, protect } from "../middleware/authMiddleware.js";
-import { createBrand, getBrandById, getBrands, updateBrand, deleteBrand } from "../controllers/brandController.js";
+import { createBrand, getBrandById, getBrands, updateBrand, deleteBrand } from "../controllers/brandController.js"; 
 
 const router = express.Router();
 
 // Home Route
 router.route("/").get(getBrands).post(protect, admin, createBrand );
 
-// :id
+// :id 
 router
     .route("/:id")
     .get(getBrandById)
