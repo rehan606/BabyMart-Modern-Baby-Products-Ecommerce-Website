a9522254272a4a9cfb73ad72be5c0d746a9eda40
Rehan <rehanlemu@gmail.com>
2025-11-06 22:23:39 +0600

    create category route by id

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { admin, protect } from "../middleware/authMiddleware.js";
-import { createCategory, getCategories } from "../controllers/categoryController.js";
+import { createCategory, deleteCategory, getCategories, getCategoryById, updateCategory } from "../controllers/categoryController.js";
 const router = express.Router();
 
-router.route("/").get(getCategories).post(protect, admin, createCategory);
+router.route("/")
+    .get(getCategories)
+    .post(protect, admin, createCategory);
 
+router.route("/:id")
+    .get(protect, getCategoryById)
+    .put(protect, admin, updateCategory)
+    .delete(protect, admin, deleteCategory);
 
 export default router;
