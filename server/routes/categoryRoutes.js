9f5e5d8a6033ef0115e76d2716602514664326af
Rehan <rehanlemu@gmail.com>
2025-11-06 21:36:38 +0600

    create cateory controller file

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { admin, protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").get(getCategories).post(protect, admin, createCategory);
