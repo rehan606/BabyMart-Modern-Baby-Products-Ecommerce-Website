ecf14cb10bdbddcefaf92eb78c426a4ebf745c50
Rehan <rehanlemu@gmail.com>
2025-11-06 20:16:12 +0600

    Added all state for category.tsx file

--- admin/src/pages/Categories.tsx
+++ admin/src/pages/Categories.tsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
 
+type Category = {
+  _id: string;
+  name: string;
+  image?: string;
+  categoryType: "Featured" | "Hot Categories" | "Top Categories";
+  createdAt: string;
+};
 
 const Categories = () => {
+
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(10);
+  const [totalPages, setTotalPages] = useState(1);
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+  const [refreshing, setRefreshing] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
+  const [formLoading, setFormLoading] = useState(false);
+
+
   return (
     <div>
       Categories
